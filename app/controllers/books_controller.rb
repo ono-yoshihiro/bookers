c24e9326308f7251d174f438779aba9731df31b4
EC2 Default User <ec2-user@ip-172-31-11-44.ap-northeast-1.compute.internal>
2022-07-28 15:01:24 +0000

    [Finish] 7/29

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,33 @@
 class BooksController < ApplicationController
-  def new #indexページでnewアクションを行うため、new.html.erbは作成していないが
+  def new
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id)
+  end
+
+  def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
   end
 
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+
+  private
+  def book_params
+    params.permit(:title, :body)  #.require(:book)を記述するとエラーになるのはなぜ？
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root :to => 'homes#top'
   get 'books' => 'books#index'
-  post 'boooks' => 'books#create
-  get 'books/:id' => 'books#show', as: 'list'
-  get 'books/:id/edit' => 'books#edit', as: 'edit_list'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
+  post 'books' => 'books#create'
+  get 'books/:id' => 'books#show', as: 'book'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+endroo
