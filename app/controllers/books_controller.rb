1fd2ed6f1d3ec3828e2592117d79a5e145e21109
EC2 Default User <ec2-user@ip-172-31-11-44.ap-northeast-1.compute.internal>
2022-08-01 14:00:30 +0000

    [Finish] 3-12

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,19 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = 'Book was successfully created.'
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -18,6 +21,17 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = 'Book was successfully updated.'
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
@@ -28,6 +42,6 @@ class BooksController < ApplicationController
 
   private
   def book_params
-    params.permit(:title, :body)  #.require(:book)を記述するとエラーになるのはなぜ？
+    params.require(:book).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
+  resources :books
   root :to => 'homes#top'
-  get 'books' => 'books#index'
-  post 'books' => 'books#create'
-  get 'books/:id' => 'books#show', as: 'book'
-  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
-  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
-endroo
+end
